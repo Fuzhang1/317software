b1f276cea02f24c0ed5ee76003d3d627b8508e67
LAPTOP-42H3AIHH\86183 <2054163771@qq.com>
2022-11-22 21:33:26 +0800

    try

--- 317soft/tos/app/src/main/java/com/example/a317soft/bean/ItemBean.java
+++
@@ -1,6 +0,0 @@
-package com.example.a317soft.bean;
-
-public class ItemBean {
-    public int icon;
-    public String title;
-}
--- 317soft/tos/app/src/main/java/com/example/a317soft/bean/ResourceBean.java
+++
@@ -1,7 +0,0 @@
-package com.example.a317soft.bean;
-
-public class ResourceBean {
-    public int icon;
-    public String title;
-    public String introduction;
-}
